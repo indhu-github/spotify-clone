c362347ccc272097783a49544f7f049b4d3cf816
indhu-github <indhusworkmail@gmail.com>
2021-02-25 21:06:19 +0530

    added login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -14,6 +17,10 @@ function App() {
 
     if (_token) {
       setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log("person", user);
+      });
     }
 
     console.log("I HAVE A TOKEN>>>", _token);
