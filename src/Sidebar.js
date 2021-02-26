4a752f27e731ba7c884df244b922b2c2e65f7a70
indhu-github <indhusworkmail@gmail.com>
2021-02-26 21:30:56 +0530

    added styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>I am the sidebar</h1>
+      <img
+        className="sidebar_logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <SidebarOption title="Home" />
+      <SidebarOption title="Search" />
+      <SidebarOption title="Your Library" />
     </div>
   );
 }
