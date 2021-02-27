28f3a23110f386ed11c8b49d9876998cd3bc5581
indhu-github <indhusworkmail@gmail.com>
2021-02-27 21:27:48 +0530

    added styling

--- src/App.js
+++ src/App.js
@@ -31,9 +31,16 @@ function App() {
           user: user,
         });
       });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
     }
 
-    console.log("I HAVE A TOKEN>>>", _token);
+    //console.log("I HAVE A TOKEN>>>", _token);
   }, []);
 
   // console.log("person", user);
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,8 +4,11 @@ import SidebarOption from "./SidebarOption";
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+import { useDataLayerValue } from "./DataLayer";
 
 function Sidebar() {
+  const [{ playlists }, dispatch] = useDataLayerValue();
+  console.log("playlists:", playlists);
   return (
     <div className="sidebar">
       <img
@@ -16,6 +19,12 @@ function Sidebar() {
       <SidebarOption Icon={HomeIcon} title="Home" />
       <SidebarOption Icon={SearchIcon} title="Search" />
       <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+      <br />
+      <strong className="sidebar_title">PLAYLISTS</strong>
+      <hr />
+      {playlists?.items?.map((playlist) => (
+        <SidebarOption title={playlist.name} />
+      ))}
     </div>
   );
 }
