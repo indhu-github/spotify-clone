30a527c7f24628fc99bdfc48be1aea71f5ec40e4
indhu-github <indhusworkmail@gmail.com>
2021-02-28 17:08:42 +0530

    Added Header which is part of Body component

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,11 @@
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import { Avatar } from "@material-ui/core";
+import { useDataLayerValue } from "./DataLayer";
 
 const Header = () => {
+  const [{ user }, dispatch] = useDataLayerValue();
+
   return (
     <div className="header">
       <div className="header_left">
@@ -10,7 +13,7 @@ const Header = () => {
         <input placeholder="Search for Artists,Songs and Albums" type="text" />
       </div>
       <div className="header_right">
-        <Avatar src="" alt="I" />
+        <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
         <h4>Indhu</h4>
       </div>
     </div>
